b944e5c70d31889de8d05c015503c06daf246aa9
asomala <68791501+abhisomala@users.noreply.github.com>
2022-01-11 23:53:22 +0000

    Added a conditional If both smile or neither smile then we are in trouble otherwise we are good

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 ASmile = input ("Is ASmile smiling? Please capatlize first letter! Yes or No:  ")
 BSmile = input ("Is BSmile smiling? Please captilize first letter! Yes or No:  ")
+
+#Asking wheter ASmile and BSmile is smiling
+
+if ASmile  == "Yes" and BSmile == "Yes":
+  print("We are in trouble!")
+elif ASmile == "No" and BSmile == "No":
+  print ("We are in trouble!")
+else:
+  print("Relax, we're good")
+# If both smile or neither smile is bad otherwise we are good
